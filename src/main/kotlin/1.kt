e2ffa1f6e757b609339b92180f9d93b9d7370ed9
Martin Maier-Moessner <martin.maier.moessner@gmail.com>
2024-12-01 11:11:00 +0100

    day 1

--- src/main/kotlin/1.kt
+++ src/main/kotlin/1.kt
@@ -1,19 +1,36 @@
+import kotlin.math.absoluteValue
+
 fun main() {
-    val testInput = ""
+    val testInput = "3   4\n" +
+            "4   3\n" +
+            "2   5\n" +
+            "1   3\n" +
+            "3   9\n" +
+            "3   3"
+
+    fun getInput(lines: List<String>): Pair<List<Int>, List<Int>> {
+        val input = lines.map { it.split("   ").map { s -> s.toInt() } }
+        val firstList = input.map { it[0] }.sorted()
+        val secondList = input.map { it[1] }.sorted()
+        return firstList to secondList
+    }
 
     fun solve1(lines: List<String>): Long {
-        return -1
+        val (firstList, secondList) = getInput(lines)
+        return firstList.mapIndexed { index, value -> (secondList[index] - value).absoluteValue }.sum().toLong()
     }
 
     fun solve2(lines: List<String>): Long {
-        return -1
+        val (firstList, secondList) = getInput(lines)
+        val countList = secondList.groupBy { it }.mapValues { it.value.size  }
+        return firstList.sumOf { value -> value * (countList[value] ?: 0) }.toLong()
     }
-
+    
     header(1)
-    test(::solve1, testInput, 1337)
+    test(::solve1, testInput, 11)
     solve(::solve1)
 
     header(2)
-    test(::solve2, testInput, 1337)
+    test(::solve2, testInput, 31)
     solve(::solve2)
 }
